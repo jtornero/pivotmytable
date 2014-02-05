083dc94e13cdb6d31a07d84d4c4c5b801df53f57
jtornero <jtorlistas@gmail.com>
2014-02-06 00:31:32 +0100

    Initial deployment of PivotMyTable

--- README.md
+++ README.md
@@ -39,67 +39,8 @@ Additionally, if you really feel in the mood of rewarding me, just feel free for
 Installation and usage
 ======================
 
-PivotMyTable requires the PostgreSQL extension tablefunc and the language PL/Python installed in the database to work.
-
-- *Installation of tablefunc extension:* Please check PostgreSQL documentation pages for extensions installation in <a href="http://www.postgresql.org/docs/9.3/static/contrib.html" target="_blank">http://www.postgresql.org/docs/9.3/static/contrib.html</a>. Check for the correct manual page for your version of PostgreSQL.
-
-- *Installation of PL/Python language:* Please check PostgreSQL documentation pages for the installation of PL/Python <a href="http://www.postgresql.org/docs/9.0/static/plpython.html" target="_blank">http://www.postgresql.org/docs/9.3/static/plpython.html</a>. Check for the correct manual page for your version of PostgreSQL.
-        
-
-The usage of PivotMyTable is simple: Providing that you have a table *myinfo* like:
+Please visit my page about PivotMyTable in my personal web:
 
 
-
-|player|tool|round|hits|
-|------|----|:-----:|:----:|
-|Pepito|Hammer|Rd1|12|
-|Pepito|Hammer|Rd2|13|
-|Pepito|Hammer|Rd2|4|
-|Pepito|Wrench|Rd5|1|
-|Manu|Wrench|Rd1|12|
-|Manu|Wrench|Rd1|16|
-|Manu|Hammer|Rd2|3|
-|Richal|Hammer|Rd3|42|
-|Richal|Hammer|Rd1|17|
-|Richal|Hammer|Rd4|22|
-|Richal|Hammer|Rd2|15|
-|Richal|Hammer|Rd1|17|
-
-You can issue this query:
-
-<pre><code>select * from pivotmytable('myinfo','pivotedinfo','player,tool','round','sum',sort_order:='asc');</code></pre>
-
-To get a pivoted table like this:
-
-|player|tool|Rd1|Rd2|Rd3|Rd4|Rd5|
-|------|----|:---:|:---:|:---:|:---:|:---:|
-|Pepito|Hammer|12|17|0|0|0|
-|Pepito|Wrench|0|0|0|0|1|
-|Manu|Hammer|0|3|0|0|0|
-|Manu|Wrench|28|0|0|0|0|
-|Richal|Hammer|34|15|42|22|0|
-
-
-Function Parameters/Options
-===========================
-
-- **input_table (varchar):**
-    Name of the table to get data to pivot.
-- **output_table(varchar):**
-    Name for the output table.
-- **group_fields(varchar):**
-    Name of the column(s) for categorizing the data. Unlike the native *tablefunc* module functions *crosstab*, it accepts multiple columns.
-- **pivot_field(varchar):** 
-    Column to be pivoted, must be a single column.
-- **value_field(varchar):**
-Data to aggregate for the pivot field. It is expected for value_field to be a numeric (int, float, etc) field.
-- **agg_func(varchar):**
-Aggregate function to apply to the data from value_field. It  must be specified without brackets. It's user responsability to check for function/data type compatibility and resulting data. So far it has been tested with sum().
-- **as_percentage(bool):**
-This option enables the output as percentage of each output columns over its overall sum BY ROW. It defaults to FALSE, so to enable percentage calculation set it to TRUE.
-- **sort_order(varchar):**
-This option enables sorting the resulting pivoted columns. You can specify 'asc', 'desc'. It defaults to 'no_sorting'.
-- **drop_ex_tbl (boolean):**
-pivotMyTablee checks if the output table specified with the parameter output_table exists before further processing. Setting this option to TRUE makes possible to automatically drop the existing table. By default, drop_ex_tbl is set to FALSE so the function exists with a warning if output_table already exists in the database.
 
 
---
+++ pivotmytable.sql
@@ -0,0 +1,242 @@
+/* pivotMyTable An improved crosstab function for PostgreSQL.
+ 
+ Copyright 2014 Jorge Tornero Nunez http://imasdemase.com
+ 
+ pivotMyTable is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ pivotMyTable is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with pivotMyTable.  If not, see <http://www.gnu.org/licenses/>.
+
+FUNCTION DESCRIPTION
+====================
+pivotMyTablee is a PL/Python function for use in PostgreSQL servers. Its aim is
+to get crosstab/pivoted tables in a more friendly way that PostgreSQL module
+tablefunc does with its crosstab series functions and in fact it behaves in the
+end as a proxy for tablefunc functions.
+
+pivotMyTablee makes it possible in the same way that other available solutions,
+automating the creation the queries that the tablefunc crosstab functions need to work.
+
+Also, pivotMyTablee makes possible to directly get percentages,
+as well as get rid of null values in the oputput tables.
+
+FUNCTION PARAMETERS/OPTIONS
+===========================
+input_table (varchar):  Name of the table to get data to pivot.
+output_table(varchar):  Name for the output table.
+group_fields(varchar):  Name of the column(s) for categorizing the data. Unlike the
+                        native tablefunc module functions crosstab, it accepts multiple
+                        columns.
+pivot_field(varchar):   Column to be pivoted, must be a single column.
+value_field(varchar):   Data to aggregate for the pivot field. It is expected for
+                        value_field to be a numeric (int, float, etc) field.
+agg_func(varchar):      Aggregate function to apply to the data from value_field. It
+                        must be specified without brackets.
+                        It's user responsability to check for function/data type
+                        compatibility and resulting data. So far it has been tested
+                        with sum.
+as_percentage(bool):    This option enables the output as percentage of each output
+                        columns over its overall sum BY ROW. It defaults to FALSE, so
+                        to enable percentage calculation set it to TRUE.
+sort_order(varchar):    This option enables sorting the resulting pivoted columns.
+                        You can specify 'asc', 'desc'. It defaults to 'no_sorting'
+drop_ex_tbl (boolean):  pivotMyTablee checks if the output table specified with the
+                        parameter output_table exists before further processing.
+                        Setting this option to TRUE makes possible to automatically
+                        drop the existing table. By default, drop_ex_tbl is set to
+                        FALSE so the function exists with a warning if output_table
+                        already exists in the database.
+                        
+*/
+
+CREATE OR REPLACE FUNCTION pivotmytable (input_table varchar,
+                                   output_table varchar,
+                                   group_fields varchar,
+                                   pivot_field varchar,
+                                   value_field varchar,
+                                   agg_func varchar,
+                                   as_percentage bool default false,
+                                   sort_order varchar default 'no_sorting',
+                                   drop_ex_tbl boolean default false)
+  RETURNS varchar
+  LANGUAGE plpythonu
+
+AS $$
+    """Function pivotMyTable"""
+    
+    import math
+    
+    # Time to check if input_table,group_fields, pivot_field and value_field parameters are OK
+    
+    tablesQuery = plpy.execute("select table_name from information_schema.tables")
+    tableList = [tablename["table_name"] for tablename in tablesQuery]
+    
+    inputTableColumnsQuery=plpy.execute("select column_name from information_schema.columns where table_name='%s'" %input_table)
+    inputTableColumnsList = [col["column_name"] for col in inputTableColumnsQuery]
+    
+    if input_table not in tableList:
+        return "Your input table %s does not exist.  Please check and try again." %input_table
+        
+    for parameter in group_fields.split(','):
+        if parameter not in inputTableColumnsList:
+            return ("The column %s specified in group_fields does not exist in the table %s. Please check and try again." %(parameter,input_table))
+    
+     
+    if pivot_field not in inputTableColumnsList:
+        return ("The column %s specified in group_field does not exist in the table %s. Please check and try again." %(pivot_field,input_table))
+    if value_field not in inputTableColumnsList:
+        return ("The column %s specified in value_field does not exist in the table %s. Please check and try again." %(value_field,input_table))
+    
+    # Checking for percentage and aggregate function compatibility
+    
+    if agg_func not in ('sum','count','avg'):
+        return "Aggregate function %s is not compatible/tested. Aborting" %agg_func
+    
+    if as_percentage and (agg_func!='sum'):
+            return "Percentage calculation and aggregate function %s are not compatible. Aborting" %agg_func
+    
+    
+    # Checks if the output table name already exists in the database
+        
+    if (output_table in tableList):
+        if (drop_ex_tbl):
+            plpy.execute("drop table %s" %output_table)
+        else:
+            return "The table %s already exists in the database.  Please check and try again." %output_table
+        
+        
+    # Management of sort order parameter
+    
+    if sort_order == 'asc':
+        ordering="(select %s, %s(%s) as ordervalue from %s group by %s order by 2 asc) as t2"%(pivot_field,agg_func,value_field,input_table,pivot_field)
+    elif sort_order == 'desc':
+        ordering="(select %s, %s(%s) as ordervalue from %s group by %s order by 2 desc) as t2"%(pivot_field,agg_func,value_field,input_table,pivot_field)
+    elif sort_order == 'no_sorting':
+        ordering="(select distinct %s from %s order by 1) as t2"%(pivot_field,input_table)
+    else:
+        return "Wrong sort parameter specification. It must be 'asc', 'desc' or no. Please check and try again."
+        
+    # GATHERING OF THE PIVOTED COLUMN NAMES AND THEIR DATA TYPES.
+    
+    # First we get the names of the destination fields, which are the values present in
+    # the pivot_field column
+    
+    destColumns = plpy.execute("select %s as columns from %s " %(pivot_field, ordering))
+    
+    # We need this for the final field splitting
+    destColumns2 = plpy.execute("select distinct %s as column from %s order by 1" %(pivot_field,input_table))
+    
+    # Now we get the column type. Because all the output pivoted columns
+    # will have the same data type than input columns, its definition is
+    # simple in the case of integer/double columns, but for numeric columns
+    # we need to get both the precision (number of decimals) and the maximum
+    # value of its aggregate, to prevent overflows when aggregating the data
+    
+    # First we make the query to information_schema.columns. 
+    
+    columnPropertiesQuery = plpy.execute("select data_type,numeric_scale from information_schema.columns where table_name = '%s' and column_name='%s'" %(input_table,value_field))
+    
+    # Due to the nature of the PLyResult object returned by plpy.execute,
+    # getting the columns properties is a little tricky
+    
+    columnProperties = [property for property in columnPropertiesQuery]
+    
+    columnType = columnProperties[0]["data_type"]
+    columnScale = columnProperties[0]["numeric_scale"]
+        
+    # Because the percentage values range from 0 to 1,
+    # we need to make room for more decimal numbers for 
+    # precision not be lost
+    
+    if as_percentage:
+        fieldType = "numeric(6,5)"
+    else:
+        if columnType == 'numeric' or agg_func =='avg':
+            if columnScale == None:
+                columnScale = 8
+            maxFieldValue = plpy.execute("select %s(%s) as agg_funcres from %s" %(agg_func, value_field,input_table))
+            magnitudeOrder = math.log10(maxFieldValue[0]["agg_funcres"])
+            numericFieldWidth = magnitudeOrder + columnScale + 1
+            fieldType = ("numeric(%i, %i)" %(numericFieldWidth, columnScale))
+            
+        elif columnType in ('bigint','smallint','integer','real','double precision'):
+            fieldType = columnType
+        else:
+            return "Your pivot column %s is not of a numeric type.  Please check and try again." %pivot_field
+       
+    flds = ['"%s" %s'%(destColumn["column"],fieldType) for destColumn in destColumns2]
+    fields = ','.join(flds)
+    
+    gfields=group_fields.split(',')
+    groupingColumns=list()
+    idx=1
+    for field in gfields:
+        columnType = plpy.execute("select data_type from information_schema.columns where table_name = '%s' and column_name='%s'" %(input_table,field))
+        tc=[columna["data_type"] for columna in columnType]
+        
+        # We need this to recover the grouped fields after crosstab execution 
+        
+        nc = ("split_part(trim(joinedcols,'()'),',',%i)::%s as %s" %(idx,tc[0],field))
+               
+        groupingColumns.append (nc)
+        idx += 1
+    groupingColumns=','.join(groupingColumns)
+
+    # Construction of the queries to be passed to crosstab function
+
+    if as_percentage:
+        # If we want percentages instead of absolute values, we need to
+        # create a new table with the percentages, and the queries
+        # that are passed to crosstab funcion are silightly different
+        # Notice the use of row(), that makes possible to have crosstabs
+        # grouped by more than one single field, but to this to be done,
+        # we have had to do a lot of tricks before (see the use of
+        # split_part above)
+        
+        groupbyclause=group_fields + ',' + value_field
+
+        plpy.execute("""create temporary table intertable as
+            (select %s,%s,%s/%s(%s::numeric) over (partition by %s) as percentages from %s group by %s,%s,%s order by %s)""" 
+            %(group_fields,pivot_field,value_field,agg_func,value_field,group_fields,input_table,group_fields,pivot_field,value_field,group_fields))
+        firstQuery=("""select distinct row(%s),%s,%s(percentages) from intertable
+            group by %s,%s order by 1,2"""
+            %(group_fields,pivot_field,agg_func,group_fields,pivot_field)) 
+        secondQuery = ("select distinct %s from intertable order by 1" %(pivot_field))
+        
+    else:
+        # For absolute values, the queries passed to crosstab are simpler
+    
+        aggrfun=("%s(%s)" %(agg_func,value_field))
+        groupbyclause=group_fields
+        firstQuery=("select distinct row(%s),%s,%s from %s group by %s,%s order by 1,2" %(group_fields,pivot_field,aggrfun,input_table,groupbyclause,pivot_field))
+        secondQuery = ("select distinct %s from %s order by 1" %(pivot_field,input_table))
+    
+    # We create the sentence for output fields for crosstab
+    crossTabQuery = ("joinedcols varchar,%s"%(fields))
+    
+    # This trick makes possible to get rid of the null values in the pivoted tables.
+    # TODO: Consider to make it optional, with a parameter for it.
+    
+    replaceZeros = ['coalesce("{0}",0) as "{0}"'.format(destColumn["columns"]) for destColumn in destColumns]
+    replaze0 = ','.join(replaceZeros)
+    
+    # And now, we put everything together and execute the query 
+    finalQuery = ("create table %s as (select %s,%s from crosstab('%s','%s') as newtable(%s))" %(output_table,groupingColumns,replaze0,firstQuery,secondQuery,crossTabQuery))  
+    plpy.execute(finalQuery)
+    
+    # A little cleanup may be necessary
+    if as_percentage:
+        pass
+        plpy.execute("drop table intertable")
+
+    return "Your pivoted table %s has been created." %(output_table)
+  
+$$;
